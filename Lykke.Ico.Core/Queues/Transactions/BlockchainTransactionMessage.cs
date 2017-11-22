40e8677e1307e357e0802c189e57b8fa9e709565
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2017-11-22 14:20:10 +0300

    Improved blockchain transaction message

--- Lykke.Ico.Core/Queues/Transactions/BlockchainTransactionMessage.cs
+++ Lykke.Ico.Core/Queues/Transactions/BlockchainTransactionMessage.cs
@@ -7,9 +7,25 @@ namespace Lykke.Ico.Core.Queues.Transactions
     [QueueMessage(QueueName = Consts.Transactions.Queues.BlockchainTransaction)]
     public class BlockchainTransactionMessage : IMessage
     {
+        /// <summary>
+        /// Block hash
+        /// </summary>
+        public string BlockId { get; set; }
+
+        public DateTimeOffset BlockTimestamp { get; set; }
+
+        /// <summary>
+        /// Outpoint for BTC | hash for ETH
+        /// </summary>
         public string TransactionId { get; set; }
+        
         public string DestinationAddress { get; set; }
+
+        /// <summary>
+        /// Bitcoin | Ether
+        /// </summary>
         public CurrencyType CurrencyType { get; set; }
+
         public decimal Amount { get; set; }
     }
 }
