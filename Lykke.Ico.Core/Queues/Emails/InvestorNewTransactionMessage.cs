b5cfd6ecfb7ba5d08edcf6cd60c3d61cf683294d
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2017-11-23 12:31:15 +0300

    Replaced Amount and CurrencyType pair with single Payment property. Payment property should contain user-friendly formatted payment details.

--- Lykke.Ico.Core/Queues/Emails/InvestorNewTransactionMessage.cs
+++ Lykke.Ico.Core/Queues/Emails/InvestorNewTransactionMessage.cs
@@ -5,17 +5,17 @@
     {
         public string EmailTo { get; set; }
         public int Attempts { get; set; }
-        public CurrencyType CurrencyType { get; set; }
-        public string Amount { get; set; }
+        public string Payment { get; set; }
+        public string TransactionLink { get; set; }
 
-        public static InvestorNewTransactionMessage Create(string email, CurrencyType currencyType, string amount)
+        public static InvestorNewTransactionMessage Create(string emailTo, string payment, string transactionLink)
         {
             return new InvestorNewTransactionMessage
             {
-                EmailTo = email,
+                EmailTo = emailTo,
                 Attempts = 0,
-                CurrencyType = currencyType,
-                Amount = amount
+                Payment = payment,
+                TransactionLink = transactionLink
             };
         }
     }
