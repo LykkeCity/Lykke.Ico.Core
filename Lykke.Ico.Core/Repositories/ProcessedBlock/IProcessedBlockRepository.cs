baf0ec62899a41e92268fae54f1c30169d9dc201
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2017-11-24 10:37:28 +0300

    Added ProcessedBlock repository

---
+++ Lykke.Ico.Core/Repositories/ProcessedBlock/IProcessedBlockRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Lykke.Ico.Core.Repositories.ProcessedBlock
+{
+    public interface IProcessedBlockRepository
+    {
+        Task<int> GetLastProcessedBlockAsync(CurrencyType currencyType, string networkName = null);
+
+        Task SetLastProcessedBlockAsync(int height, CurrencyType currencyType, string networkName = null);
+    }
+}
---
+++ Lykke.Ico.Core/Repositories/ProcessedBlock/ProcessedBlockEntity.cs
@@ -0,0 +1,9 @@
+﻿using Lykke.AzureStorage.Tables;
+
+namespace Lykke.Ico.Core.Repositories.ProcessedBlock
+{
+    internal class ProcessedBlockEntity : AzureTableEntity
+    {
+        public int Height { get; set; }
+    }
+}
