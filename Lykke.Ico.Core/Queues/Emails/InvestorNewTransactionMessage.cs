efd26e98717141c54c52ccaee313526fabafa054
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2017-12-13 13:32:48 +0300

    Added KYC and investment fields to new transaction message

--- Lykke.Ico.Core/Queues/Emails/InvestorNewTransactionMessage.cs
+++ Lykke.Ico.Core/Queues/Emails/InvestorNewTransactionMessage.cs
@@ -6,5 +6,10 @@
         public string EmailTo { get; set; }
         public string Payment { get; set; }
         public string TransactionLink { get; set; }
+        public bool KycRequired { get; set; }
+        public string KycLink { get; set; }
+        public bool MoreInvestmentRequired { get; set; }
+        public decimal MinAmount { get; set; }
+        public decimal InvestedAmount { get; set; }
     }
 }
