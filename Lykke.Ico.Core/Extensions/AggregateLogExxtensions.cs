0bd7e0cbe71c782b0d9188d4b336b3fd421f61b8
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2017-12-18 11:44:01 +0300

    Added common ICO log registration method

---
+++ Lykke.Ico.Core/Extensions/AggregateLogExxtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lykke.Logs;
+using Lykke.Logs.Slack;
+using Lykke.SlackNotifications;
+
+namespace Common.Log
+{
+    public static class AggregateLoggerExxtensions
+    {
+        /// <summary>
+        /// Adds logger which writes ICO messages to personal Slack channel.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="slackNotificationsSender">Service which sends notifications to Slack</param>
+        /// <param name="channel">Name of channel in SlackIntegration settings section ("Ico" by default)</param>
+        /// <param name="logLevel">Level of message to log (Error | FatalError by default)</param>
+        public static void AddIcoSlackLog(this AggregateLogger self, ISlackNotificationsSender slackNotificationsSender, 
+            string channel = "Ico", 
+            LogLevel logLevel = LogLevel.Error | LogLevel.FatalError)
+        {
+            self.AddLog(LykkeLogToSlack.Create(slackNotificationsSender, channel, logLevel));
+        }
+    }
+}
