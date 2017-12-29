ed01d7f96f799c540940530e9cd18d24ce206e36
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2017-12-29 19:01:19 +0300

    Add encryption helper class

---
+++ Lykke.Ico.Core.Tests/EncryptionHelperTests.cs
@@ -0,0 +1,21 @@
+﻿using Lykke.Ico.Core.Helpers;
+using Xunit;
+
+namespace Lykke.Job.IcoJobEmail.Tests
+{
+    public class EncryptionHelperTests
+    {
+        [Fact]
+        public void MustEncryptDecrypt()
+        {
+            var message = "Example test";
+            var key = "E546C8DF278CD5931069B522E695D4F2";
+            var iv = "1234567890123456";
+
+            var encrypted = EncryptionHelper.Encrypt(message, key, iv);
+            var decprypted = EncryptionHelper.Decrypt(encrypted, key, iv);
+
+            Assert.Equal(message, decprypted);
+        }
+    }
+}
---
+++ Lykke.Ico.Core/Helpers/EncryptionHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lykke.Ico.Core.Helpers
+{
+    public class EncryptionHelper
+    {
+        public static string Encrypt(string message, string key, string iv)
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            var keyBytes = Encoding.UTF8.GetBytes(key);
+            var ivBytes = Encoding.UTF8.GetBytes(iv);
+
+            using (var aes = Aes.Create())
+            {
+                using (var encryptor = aes.CreateEncryptor(keyBytes, ivBytes))
+                {
+                    using (var msEncrypt = new MemoryStream())
+                    {
+                        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            csEncrypt.Write(messageBytes, 0, messageBytes.Length);
+                            csEncrypt.FlushFinalBlock();
+
+                            var encryptedBytes = msEncrypt.ToArray();
+
+                            return WebUtility.UrlEncode(Convert.ToBase64String(encryptedBytes));
+                        }
+                    }
+                }
+            }
+        }
+
+        public static string Decrypt(string encodedMessage, string key, string iv)
+        {
+            var decodedMessage = WebUtility.UrlDecode(encodedMessage);
+            var message = Convert.FromBase64String(decodedMessage);
+            var keyBytes = Encoding.UTF8.GetBytes(key);
+            var ivBytes = Encoding.UTF8.GetBytes(iv);
+
+            using (var aes = Aes.Create())
+            {
+                using (var decryptor = aes.CreateDecryptor(keyBytes, ivBytes))
+                {
+                    using (var msDecrypt = new MemoryStream(message))
+                    {
+                        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (var srDecrypt = new StreamReader(csDecrypt))
+                            {
+                                return srDecrypt.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
