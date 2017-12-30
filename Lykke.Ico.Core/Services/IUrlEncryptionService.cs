5d6a968656684295d113cda07d2aeaa4efea7cbb
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2017-12-30 13:33:02 +0300

    Add UrlEncryptionService

---
+++ Lykke.Ico.Core/Services/IUrlEncryptionService.cs
@@ -0,0 +1,8 @@
+﻿namespace Lykke.Ico.Core.Services
+{
+    public interface IUrlEncryptionService
+    {
+        string Encrypt(string message);
+        string Decrypt(string message);
+    }
+}
---
+++ Lykke.Ico.Core/Services/UrlEncryptionService.cs
@@ -0,0 +1,26 @@
+﻿using Lykke.Ico.Core.Helpers;
+
+namespace Lykke.Ico.Core.Services
+{
+    public class UrlEncryptionService
+    {
+        private readonly string _key;
+        private readonly string _iv;
+
+        public UrlEncryptionService(string key, string iv)
+        {
+            _key = key;
+            _iv = iv;
+        }
+
+        public string Encrypt(string message)
+        {
+            return EncryptionHelper.Encrypt(message, _key, _iv);
+        }
+
+        public string Decrypt(string message)
+        {
+            return EncryptionHelper.Decrypt(message, _key, _iv);
+        }
+    }
+}
