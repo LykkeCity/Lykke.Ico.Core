acf03c1ee9a45aab50b9ae7179259d9caad53fd3
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2018-01-11 15:10:53 +0300

    Add KycService

---
+++ Lykke.Ico.Core/Services/IKycService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Lykke.Ico.Core.Services
+{
+    public interface IKycService
+    {
+        Task<string> GetKycLink(string email, string kycId);
+    }
+}
---
+++ Lykke.Ico.Core/Services/KycService.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+using Lykke.Ico.Core.Repositories.CampaignSettings;
+using System.Threading.Tasks;
+
+namespace Lykke.Ico.Core.Services
+{
+    public class KycService : IKycService
+    {
+        private readonly ICampaignSettingsRepository _campaignSettingsRepository;
+        private readonly IUrlEncryptionService _urlEncryptionService;
+
+        public KycService(ICampaignSettingsRepository campaignSettingsRepository,
+            IUrlEncryptionService urlEncryptionService)
+        {
+            _campaignSettingsRepository = campaignSettingsRepository;
+            _urlEncryptionService = urlEncryptionService;
+        }
+
+        public async Task<string> GetKycLink(string email, string kycId)
+        {
+            var settings = await _campaignSettingsRepository.GetAsync();
+            var kycMessage = new { campaignId = settings.KycCampaignId, email = email, kycId = kycId };
+            var kycEncryptedMessage = _urlEncryptionService.Encrypt(kycMessage.ToJson());
+            var kycLink = settings.KycLinkTemplate.Replace("{kycEncryptedMessage}", kycEncryptedMessage);
+
+            return kycLink;
+        }
+    }
+}
