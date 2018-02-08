4ae5fbb4d2ae9043fb313d220e276cdd68f9ba6c
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2018-02-08 14:34:30 +0300

    Fix kyc notification email

--- Lykke.Ico.Core/Queues/Emails/InvestorKycNotificationMessage.cs
+++ Lykke.Ico.Core/Queues/Emails/InvestorKycNotificationMessage.cs
@@ -1,7 +1,7 @@
 ﻿namespace Lykke.Ico.Core.Queues.Emails
 {
     [QueueMessage(QueueName = Consts.Emails.Queues.InvestorKycNotification)]
-    public class InvestorKycNotificationMessage
+    public class InvestorKycNotificationMessage : IInvestorMessage
     {
         public string EmailTo { get; set; }
         public string LinkToSummaryPage { get; set; }
