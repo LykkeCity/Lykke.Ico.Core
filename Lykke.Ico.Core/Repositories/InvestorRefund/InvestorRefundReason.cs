9a21f87aa458dca05a1a560b6243e80c50943d55
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2018-02-19 16:22:06 +0300

    Add HardCapUsdExceeded tx fail reason

--- Lykke.Ico.Core/Repositories/InvestorRefund/InvestorRefundReason.cs
+++ Lykke.Ico.Core/Repositories/InvestorRefund/InvestorRefundReason.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lykke.Ico.Core.Repositories.InvestorRefund
+﻿namespace Lykke.Ico.Core.Repositories.InvestorRefund
 {
     public enum InvestorRefundReason
     {
         OutOfDates,
         PreSaleTokensSoldOut,
-        TokensSoldOut
+        TokensSoldOut,
+        HardCapUsdExceeded
     }
 }
